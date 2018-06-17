0c8513f4f005b5029ced1a6d4d6f4018eac5499f
Jesper Alf Dam <mail@jalf.dk>
2018-06-17 16:12:10 +0200

    Cleaned up tests and made generic test helpers

--- frontend-test.cc
+++ frontend-test.cc
@@ -1,75 +1,136 @@
-#include <memory>
+#include <utility>
 #include "frontend.hpp"
 #include <catch.hpp>
 
 using np::log::internal::literal;
 
-const np::log::Header hdr{np::log::internal::literal(""),
-  np::log::internal::literal(""),
-  np::log::Severity::Error};
+namespace {
+  template <typename T>
+  using FrontendArg = std::tuple<np::log::Severity, std::string_view, const T*>;
+  template <typename T>
+  using BackendArg = std::tuple<bool, std::string_view, const T*>;
 
-TEST_CASE("frontend header") {
+  const np::log::Header hdr{np::log::internal::literal(""),
+    np::log::internal::literal(""),
+    np::log::Severity::Error};
+
+  template <typename... Args>
   struct TestData {
     np::log::Header h;
+    std::string_view msg;
+    std::tuple<BackendArg<Args>...> args;
+    int call_count = 0;
   };
+  template <typename... Args>
   struct TestComposer {
-    void write(np::log::Header h, std::string_view) { ptr->h = h; }
+    void write(np::log::Header h, std::string_view msg, BackendArg<Args>... args) {
+      ptr->h = h;
+      ptr->msg = msg;
+      ptr->args = std::tuple<BackendArg<Args>...>(args...);
+      ++ptr->call_count;
+    }
 
-    TestData* ptr;
+    TestData<Args...>* ptr;
   };
-  TestData data;
-  np::log::Log<TestComposer> log({&data});
+} // namespace
+
+TEST_CASE("frontend header") {
+  TestComposer<> c;
+  std::remove_reference_t<decltype(*(c.ptr))> data;
+  c.ptr = &data;
+
+  np::log::Log<decltype(c)> log(c);
+
   np::log::scopedMessage(log, {literal("file"), literal("42"), np::log::Severity::Info})("");
+  REQUIRE(data.call_count == 1);
   CHECK(data.h.file == "file");
   CHECK(data.h.line == "42");
   CHECK(data.h.severity == np::log::Severity::Info);
 }
 
 TEST_CASE("Frontend no args") {
-  struct TestComposer {
-    void write(np::log::Header h, std::string_view msg) {
-      h2 = h;
-      msg2 = msg;
-    }
+  TestComposer<> c;
+  std::remove_reference_t<decltype(*(c.ptr))> data;
+  c.ptr = &data;
 
-    np::log::Header h2;
-    std::string_view msg2;
-  };
-  np::log::Log<TestComposer> log({});
+  np::log::Log<decltype(c)> log(c);
 
-  np::log::ScopedMessage msg(log, hdr);
+  np::log::ScopedMessage(log, hdr)("message");
+  REQUIRE(data.call_count == 1);
+  CHECK(data.msg == std::string_view("message"));
+  CHECK(std::tuple_size<decltype(data.args)>::value == 0);
 }
 
 TEST_CASE("frontend 1 arg") {
-  struct TestData {
-    np::log::Header h;
-    std::string_view msg;
-    std::tuple<bool, std::string_view, const UDT*> arg0;
-    int call_count = 0;
-  };
-  struct TestComposer {
-    void write(np::log::Header h,
-      std::string_view msg,
-      std::tuple<bool, std::string_view, const UDT*> arg0) {
-      ptr->h = h;
-      ptr->msg = msg;
-      ptr->arg0 = arg0;
-      ++ptr->call_count;
-    }
+  TestComposer<UDT> c;
+  std::remove_reference_t<decltype(*(c.ptr))> data;
+  c.ptr = &data;
 
-    TestData* ptr;
-  };
-  const auto line = __LINE__ + 1;
-  TestData data;
-  np::log::Log<TestComposer> log({&data});
+  np::log::Log<decltype(c)> log(c);
+
   UDT udt;
-  np::log::scopedMessage(log, hdr)("hello world",
-    std::tuple<np::log::Severity, std::string_view, const UDT*>{
-      np::log::Severity::All, std::string_view("argname"), &udt});
+  np::log::scopedMessage(log, hdr)(
+    "", FrontendArg<UDT>{np::log::Severity::All, std::string_view("argname"), &udt});
   REQUIRE(data.call_count == 1);
-  CHECK(data.msg == std::string_view("hello world"));
-  CHECK(std::get<0>(data.arg0) == true);
-  CHECK(std::get<1>(data.arg0) == "argname");
-  CHECK(std::get<2>(data.arg0) == &udt);
+  CHECK(std::tuple_size<decltype(data.args)>::value == 1);
+  const auto& arg0 = std::get<0>(data.args);
+  CHECK(std::get<0>(arg0) == true);
+  CHECK(std::get<1>(arg0) == "argname");
+  CHECK(std::get<2>(arg0) == &udt);
+}
+
+TEST_CASE("frontend 2 args") {
+  TestComposer<UDT, bool> c;
+  std::remove_reference_t<decltype(*(c.ptr))> data;
+  c.ptr = &data;
+
+  np::log::Log<decltype(c)> log(c);
+
+  UDT udt;
+  bool b;
+  np::log::scopedMessage(log, hdr)("",
+    FrontendArg<UDT>{np::log::Severity::All, std::string_view("arg0"), &udt},
+    FrontendArg<bool>{np::log::Severity::All, std::string_view("arg1"), &b});
+  REQUIRE(data.call_count == 1);
+  CHECK(std::tuple_size<decltype(data.args)>::value == 2);
+  const auto& arg0 = std::get<0>(data.args);
+  CHECK(std::get<0>(arg0) == true);
+  CHECK(std::get<1>(arg0) == "arg0");
+  CHECK(std::get<2>(arg0) == &udt);
+
+  const auto& arg1 = std::get<1>(data.args);
+  CHECK(std::get<0>(arg1) == true);
+  CHECK(std::get<1>(arg1) == "arg1");
+  CHECK(std::get<2>(arg1) == &b);
+}
+
+TEST_CASE("frontend 2 messages") {
+  TestComposer<char> c;
+  std::remove_reference_t<decltype(*(c.ptr))> data;
+  c.ptr = &data;
+
+  np::log::Log<decltype(c)> log(c);
+
+  char c0;
+  char c1;
+  np::log::scopedMessage(log, hdr)(
+    "msg0", FrontendArg<char>{np::log::Severity::All, std::string_view("arg0"), &c0});
+  REQUIRE(data.call_count == 1);
+  CHECK(std::tuple_size<decltype(data.args)>::value == 1);
+  const auto& arg = std::get<0>(data.args);
+  CHECK(data.msg == std::string_view("msg0"));
+  CHECK(std::get<0>(arg) == true);
+  CHECK(std::get<1>(arg) == "arg0");
+  CHECK(std::get<2>(arg) == &c0);
+
+  np::log::scopedMessage(log, hdr)(
+    "msg1", FrontendArg<char>{np::log::Severity::All, std::string_view("arg1"), &c1});
+
+  REQUIRE(data.call_count == 2);
+  CHECK(std::tuple_size<decltype(data.args)>::value == 1);
+  CHECK(data.msg == std::string_view("msg1"));
+  CHECK(std::get<0>(arg) == true);
+  CHECK(std::get<1>(arg) == "arg1");
+  CHECK(std::get<2>(arg) == &c1);
 }
 
