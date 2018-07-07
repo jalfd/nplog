ef53324554a0d91a79e14307b6adff4633b0db45
Jesper Alf Dam <mail@jalf.dk>
2018-07-07 23:34:11 +0200

    Clean up ScopedMessage a bit

--- ScopedMessage.hpp
+++ ScopedMessage.hpp
@@ -13,11 +13,11 @@ namespace np {
 
     // helpers to be used by args:
 
-    inline bool testArg(int i); // test against log's settings
-    inline bool testArg(const char* = nullptr); // assume arg uses the same debug level as the log message
+    inline bool testArg(int i) { return i < 5; }
+    inline bool testArg(const char* = nullptr) { return testArg(level); }
 
     template <typename T>
-    inline bool serialize(const char* name, const T& expr);
+    inline bool serialize(const char* name, const T& expr){ return true;}
 
     const char* getName(const char* name, const char*) { return name; }
     const char* getName(int, const char* name) { return name; }
@@ -27,12 +27,5 @@ namespace np {
 //    LogType* log;
   };
 
-#if 1
-bool ScopedMessage::testArg(int i) { return i < 5; }
-bool ScopedMessage::testArg(const char*) { return testArg(level);} // assume same debug level as the log message
-
-template <typename T>
-bool ScopedMessage::serialize(const char* name, const T& expr){ return true;}
-#endif
 } // namespace np
 #endif
