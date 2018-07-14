2d4c271b903940e7cf9b5bbbbc332b7f9467a105
Jesper Alf Dam <mail@jalf.dk>
2018-07-14 13:18:30 +0200

    Add initial Log skeleton

---
+++ Log.hpp
@@ -0,0 +1,21 @@
+#ifndef NP_LOG_LOG_HPP
+#define NP_LOG_LOG_HPP
+
+namespace np {
+  struct Log {
+    bool testMessage(int level);
+
+    // TODO: not needed in this test case, but implement where needed
+    // caller should treat the buffer as an opaque type
+    using buffer_type = std::vector<char>;
+    // caller must be able to go "give me a buffer"
+    buffer_type messageBuffer();
+    // caller must be able to go "please serialize this for me and put it in this buffer"
+    template <typename T>
+    void serialize(const char* name, const T& expr, buffer_type& buffer);
+    // caller must be able to go "ok, flush this message buffer (and take it back if you want it)
+    void submitMessage(buffer_type buffer);
+  };
+
+} // namespace np
+#endif
