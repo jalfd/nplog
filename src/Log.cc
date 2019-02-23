0c923bbd34db4081118299a6382c715bc8072042
Jesper Alf Dam <mail@jalf.dk>
2019-02-23 16:20:23 +0100

    Fix buffer overrun when printing

--- src/Log.cc
+++ src/Log.cc
@@ -1,8 +1,8 @@
 #include <nplog/Log.hpp>
 
 #include <algorithm>
-#include <mutex>
 #include <iostream>
+#include <mutex>
 
 namespace np {
   bool Log::testMessage(int level) { return level < 3; } // TODO: implement this properly
@@ -16,9 +16,13 @@ namespace np {
     return buf;
   }
 
-  void Log::submitMessage(buffer_type buffer) { std::cout << &buffer[0] << '\n'; }
+  void Log::submitMessage(buffer_type buffer) {
+    std::string str(buffer.begin(), buffer.end());
+    std::cout << str << '\n';
+  }
 
   void Log::releaseBuffer(buffer_type buf) {
+    buf.clear();
     std::lock_guard lock(buffer_mutex);
     buffers.push_back(buf);
   }
