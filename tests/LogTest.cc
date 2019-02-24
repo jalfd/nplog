eee2be4aa00ef14cebfb83d365438b8496aab5bc
Jesper Alf Dam <mail@jalf.dk>
2019-02-24 15:48:31 +0100

    Add log sink hook for custom log output

--- tests/LogTest.cc
+++ tests/LogTest.cc
@@ -19,4 +19,20 @@ TEST_CASE("Log") {
     auto b2 = log.acquireBuffer();
     CHECK(b2.empty());
   }
+
+  SECTION("Submitting a buffer sends it to the sink function") {
+    np::Log log;
+    const char* msg_start = nullptr;
+    size_t msg_len = 0;
+    np::Log::setSink([&](std::string_view msg) {
+      msg_start = &msg[0];
+      msg_len = msg.size();
+    });
+
+    auto buf = log.acquireBuffer();
+    buf.push_back('x');
+    log.submitMessage(buf);
+    CHECK(msg_start == &buf[0]);
+    CHECK(msg_len == 1);
+  }
 }
