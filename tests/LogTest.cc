60740a7f239fc6815e2e857ee6c79ca34194c943
Jesper Alf Dam <mail@jalf.dk>
2019-02-27 17:29:56 +0100

    More log tests again

--- tests/LogTest.cc
+++ tests/LogTest.cc
@@ -27,4 +27,32 @@ TEST_CASE("Log") {
     auto b2 = log.acquireBuffer();
     CHECK(b2.capacity() == 20);
   }
+
+  SECTION("Buffers are cleared on reuse") {
+    np::Log log;
+    auto buf = log.acquireBuffer();
+    buf.push_back('x');
+    log.releaseBuffer(std::move(buf));
+    auto b2 = log.acquireBuffer();
+    CHECK(b2.empty());
+  }
+
+  SECTION("Submitting a buffer sends it to the sink function") {
+    np::Log log;
+    int level = 0;
+    const char* msg_start = nullptr;
+    size_t msg_len = 0;
+    np::Log::setSink([&](int l, std::string_view msg) {
+      level = l;
+      msg_start = &msg[0];
+      msg_len = msg.size();
+    });
+
+    auto buf = log.acquireBuffer();
+    buf.push_back('x');
+    log.submitMessage(3, buf);
+    CHECK(msg_start == &buf[0]);
+    CHECK(msg_len == 1);
+    CHECK(level == 3);
+  }
 }
