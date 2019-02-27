31c7ac8266ac58a92fbf4400628d2165f03095bf
Jesper Alf Dam <mail@jalf.dk>
2019-02-27 14:27:30 +0100

    Add reentrancy test

--- tests/mocks.hpp
+++ tests/mocks.hpp
@@ -21,22 +21,23 @@ namespace {
 
   struct MockSerializer {
     using buffer_type = MockBuffer;
-    explicit MockSerializer(buffer_type* buffer) {
-      ops.emplace_back("ctor", buffer->id);
+    explicit MockSerializer(buffer_type* buffer) : bid(buffer->id) {
+      ops.emplace_back("ctor", bid);
     }
     ~MockSerializer() { ops.emplace_back("dtor", nullptr); }
 
     void prologue(std::string_view file, int line, int level, std::string_view msg) {
-      ops.emplace_back("prologue", std::make_tuple(file, line, level, msg));
+      ops.emplace_back("prologue", std::make_tuple(file, line, level, msg, bid));
     }
 
-    void epilogue() { ops.emplace_back("epilogue", nullptr); }
+    void epilogue() { ops.emplace_back("epilogue", bid); }
 
     void writeKey(std::string_view name) { ops.emplace_back("writeKey", name); }
 
     np::ValueSerializer valueSerializer() { return np::ValueSerializer{nullptr}; }
 
   private:
+    int bid;
   };
 
   inline void printOps() { // TODO: shouldn't need this
