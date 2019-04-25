769045f0a8d573ef51e1ab20feb5e37a92083c86
Jesper Alf Dam <jdam@vitalimages.com>
2019-04-25 15:30:24 +0200

    Add common library header

---
+++ include/nplog.hpp
@@ -0,0 +1,6 @@
+#include <nplog/Config.hpp>
+#include <nplog/Formatter.hpp>
+#include <nplog/Log.hpp>
+#include <nplog/macros.hpp>
+#include <nplog/ScopedMessage.hpp>
+#include <nplog/Serializer.hpp>
