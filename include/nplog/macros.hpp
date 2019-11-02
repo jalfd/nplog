982d6cd5ec03d76c94f3d776416678064ebd847c
Jesper Alf Dam <mail@jalf.dk>
2019-11-02 14:41:47 +0100

    Prefix all macros with NP_
    By default, also define unprefixed aliases for convenience, but
    internally only use the prefixed versions

--- include/nplog/macros.hpp
+++ include/nplog/macros.hpp
@@ -10,23 +10,28 @@ namespace np::log::internal {
 
 #define NP_MSVC_EXPAND_INDIRECT(m, args) m args
 
-#define ARG3(arg0, arg1, arg2) ((sm.suppressParam(arg0)) ? false : sm.addArg(arg1, arg2))
-#define ARG2(arg0, arg1) \
+#define NP_ARG3(arg0, arg1, arg2) ((sm.suppressParam(arg0)) ? false : sm.addArg(arg1, arg2))
+#define NP_ARG2(arg0, arg1) \
   ((sm.suppressParam(arg0)) ? false : sm.addArg(::np::log::internal::getArgName(arg0, #arg1), arg1))
-#define ARG1(arg0) ((sm.suppressParam()) ? false : sm.addArg(#arg0, arg0))
+#define NP_ARG1(arg0) ((sm.suppressParam()) ? false : sm.addArg(#arg0, arg0))
 
 #define INTERNAL_NP_VAR_MACRO_SELECTOR(_1, _2, _3, NAME, ...) NAME
-#define ARG(...) \
-  NP_MSVC_EXPAND_INDIRECT(INTERNAL_NP_VAR_MACRO_SELECTOR, (__VA_ARGS__, ARG3, ARG2, ARG1)) \
+#define NP_ARG(...) \
+  NP_MSVC_EXPAND_INDIRECT(INTERNAL_NP_VAR_MACRO_SELECTOR, (__VA_ARGS__, NP_ARG3, NP_ARG2, NP_ARG1)) \
   (__VA_ARGS__)
 
-#define LOG_IMPL(logger, msg_lvl, msg, ...) \
+#define NP_LOG_IMPL(logger, msg_lvl, msg, ...) \
   if (auto lvl_threshold = logger.refreshLevels(logger.knownVersion()); \
       np::log::testLevel(msg_lvl, lvl_threshold.message)) { \
     ::np::log::ScopedMessage sm(logger, __FILE__, __LINE__, msg_lvl, msg, lvl_threshold.param); \
     (void) __VA_ARGS__; \
   }
 
-#define LOG(...) NP_MSVC_EXPAND_INDIRECT(LOG_IMPL, (__VA_ARGS__, (void) nullptr))
+#define NP_LOG(...) NP_MSVC_EXPAND_INDIRECT(NP_LOG_IMPL, (__VA_ARGS__, (void) nullptr))
+
+#ifndef NP_LOG_REQUIRE_PREFIXES
+#define LOG NP_LOG
+#define ARG NP_ARG
+#endif
 
 #endif
