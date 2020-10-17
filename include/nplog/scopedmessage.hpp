71e7ac65e23c72c13ab0376c0002117cbd04be7d
Jesper Alf Dam <mail@jalf.dk>
2020-10-17 12:50:25 +0200

    Move ScopedMessage implementation to .cpp file

--- include/nplog/scopedmessage.hpp
+++ include/nplog/scopedmessage.hpp
@@ -13,20 +13,9 @@ namespace np::log {
       const char* m,
       MessageBuffer buffer,
       std::string_view log_name,
-      std::string_view logger_params_data)
-      : message_buffer(std::move(buffer)), serializer(&message_buffer), message_level(level) {
-      serializer.prologue(file, line, level, log_name, m);
-      if (!logger_params_data.empty()) {
-        serializer.startObject("static");
-        serializer.valueSerializer().writeLiteral(logger_params_data);
-        serializer.endObject();
-      }
-    }
+      std::string_view logger_params_data);
 
-    void endMessage() {
-      if (has_params) { serializer.endObject(); }
-      serializer.epilogue();
-    }
+    void endMessage();
 
     template <typename T>
     bool addParam(const char* name, T&& expr) {
@@ -52,21 +41,9 @@ namespace np::log {
   };
 
   struct NPLOG_EXPORT ScopedMessage : private ScopedMessageBase {
-    ScopedMessage(Logger& log, const char* file, int line, level_type level, const char* m)
-      : ScopedMessageBase(file,
-        line,
-        level,
-        m,
-        acquireBuffer(),
-        log.name(),
-        log.loggerParams() ? log.loggerParams()->data.contents() : std::string_view())
-      , log(log) {}
+    ScopedMessage(Logger& log, const char* file, int line, level_type level, const char* m);
 
-    ~ScopedMessage() {
-      endMessage();
-      log.submitMessage(message_level, message_buffer);
-      releaseBuffer(std::move(message_buffer));
-    }
+    ~ScopedMessage();
 
     using ScopedMessageBase::addParam;
 
